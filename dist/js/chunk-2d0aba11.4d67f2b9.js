c92139eba66aabcc63c1df1190597c8b836b8ca2
Jisan-mia <jisanmia47@gmail.com>
2022-01-08 21:48:29 +0600

    dist added

--- dist/js/chunk-2d0aba11.4d67f2b9.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0aba11"],{"15a8":function(n,e,a){"use strict";a.r(e);var t=a("7a23");function o(n,e,a,o,r,c){var d=Object(t["D"])("StudentProfile");return Object(t["v"])(),Object(t["e"])(d)}var r=a("61a5"),c={components:{StudentProfile:r["a"]},name:"Dashboard"},d=a("6b0d"),u=a.n(d);const b=u()(c,[["render",o]]);e["default"]=b}}]);
-//# sourceMappingURL=chunk-2d0aba11.4d67f2b9.js.map
